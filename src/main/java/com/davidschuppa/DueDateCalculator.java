4df9d002b608b82027ee33a3e61269e66fdba634
david <d.schuppa@gmail.com>
2019-04-25 16:20:30 +0200

    refactored isSubmitValid method

--- src/main/java/com/davidschuppa/DueDateCalculator.java
+++ src/main/java/com/davidschuppa/DueDateCalculator.java
@@ -16,14 +16,13 @@ public class DueDateCalculator {
 
     /**
      * The method validates the submission time, using LocalTime compareTo method.
-     * The comparator value is negative if less and positive if greater than the time its compared to.
      * The method checks if the given date is not on a weekend and only return true if its on a workday,
      * holidays are not considered.
      */
     public boolean isSubmitValid(LocalDateTime submission) {
         LocalTime submissionTime = submission.toLocalTime();
         DayOfWeek submissionDay = submission.toLocalDate().getDayOfWeek();
-        boolean isValidTime = submissionTime.compareTo(startHour) > 0 && submissionTime.compareTo(endHour) < 0;
+        boolean isValidTime = submissionTime.isAfter(startHour) && submissionTime.isBefore(endHour);
         boolean isNotWeekend = submissionDay != DayOfWeek.SATURDAY && submissionDay != DayOfWeek.SUNDAY;
         return isValidTime && isNotWeekend;
     }
