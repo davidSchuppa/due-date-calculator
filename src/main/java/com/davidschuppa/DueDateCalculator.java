55b049ef53db04697a33fe13d77e9136d441a7c7
david <d.schuppa@gmail.com>
2019-04-25 14:19:51 +0200

    added test case to inspect submission time

---
+++ src/main/java/com/davidschuppa/DueDateCalculator.java
@@ -0,0 +1,10 @@
+package com.davidschuppa;
+
+import java.time.LocalDateTime;
+
+public class DueDateCalculator {
+
+    public boolean isSubmitValid(LocalDateTime submitTime) {
+        return true;
+    }
+}
---
+++ src/test/java/com/davidschuppa/DueDateCalculatorTest.java
@@ -0,0 +1,25 @@
+package com.davidschuppa;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DueDateCalculatorTest {
+
+    @Test
+    public void smoke() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void submitTimeIsNotValidIfEarly() {
+        DueDateCalculator calculator = new DueDateCalculator();
+        LocalDateTime submitTime = LocalDateTime.of(LocalDate.of(2019, 4, 25), LocalTime.of(8, 30));
+
+        assertFalse(calculator.isSubmitValid(submitTime));
+    }
+}
